07bb929f3d2164b39ce9e748a5b791c6265a1a39
r4f3t <parlakrafet01@gmail.com>
2021-03-10 21:59:21 +0300

    ...

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,47 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var object1 = Singleton.GetObject();
+            var object2 = Singleton.GetObject();
+
+            if (object1==object2)
+            {
+                Console.WriteLine("Objeler eşit");
+            }
+
+
+            if (object1.Equals(object2))
+            {
+                Console.WriteLine("Objeler epeşit");
+            }
+
+
+            if (object1.GetHashCode() == object2.GetHashCode())
+            {
+                Console.WriteLine("Objeler epepeşit");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+
+
+    class Singleton
+    {
+        private Singleton()
+        {
+
+        }
+
+        private static Singleton _singleton;
+        public static Singleton  GetObject()
+        {
+            if (_singleton == null)
+            {
+                _singleton = new Singleton();
+            }
+            return _singleton;
         }
     }
 }
